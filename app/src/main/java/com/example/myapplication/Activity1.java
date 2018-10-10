adb9d4dff54284e5734e65d4ae17e7ec482697d8
AhmedRammadan <43143580+AhmedRammadan@users.noreply.github.com>
2018-10-10 15:44:17 +0200

    version 2

--- app/src/main/java/com/example/myapplication/Activity1.java
+++ app/src/main/java/com/example/myapplication/Activity1.java
@@ -3,16 +3,28 @@ package com.example.myapplication;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class Activity1 extends Fragment {
+    static String get_et;
+    EditText editText;
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.activity_1,container,false);
+        View view=inflater.inflate(R.layout.activity_1,container,false);
+        editText =view.findViewById(R.id.edit_text);
+        Button button =view.findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                get_et=editText.getText().toString();
+            }
+        });
+        return view;
     }
 }
--- app/src/main/java/com/example/myapplication/Activity3.java
+++ app/src/main/java/com/example/myapplication/Activity3.java
@@ -3,16 +3,28 @@ package com.example.myapplication;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class Activity3 extends Fragment {
+    TextView textView;
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.activity_3, container,false);
+        View view =inflater.inflate(R.layout.activity_3, container,false);
+        textView =view.findViewById(R.id.textView);
+        Button button =view.findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                textView.setText(Activity1.get_et);
+            }
+        });
+
+    return view;
     }
 }
